cabc7086358ebcc90a987ac4d28d1131a601c179
DU-ds <kenneth.collins@alumni.stonybrook.edu>
2019-11-06 22:18:41 -0500

    practiced pca b4 neural networks

--- MLPpractice.py
+++ MLPpractice.py
@@ -13,15 +13,14 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier as MLP
 from sklearn.metrics import accuracy_score
+from sklearn.decomposition import PCA
 
 iris = load_iris()
 
 features = iris.data 
 outcome = iris.target
 
-train_features, test_features = train_test_split(features)
-train_outcome, test_outcome = train_test_split(outcome)
-
+train_features, test_features, train_outcome, test_outcome = train_test_split(features, outcome, train_size = .70, random_state = 43234)
 
 centered_train_features = train_features.copy()
 ncols = centered_train_features.shape[1]
@@ -29,11 +28,18 @@ col_means = centered_train_features.mean(axis=0)
 for i in range(ncols):
     centered_train_features[:,i] = centered_train_features[:,i] - col_means[i]
 
-mlp = MLP(hidden_layer_sizes = (100))
+mlp = MLP(hidden_layer_sizes = (2*len(centered_train_features)))
 mlp.fit(centered_train_features, train_outcome)
 predicted_outcome = mlp.predict(test_features)
 score = accuracy_score(test_outcome, predicted_outcome)
 print(score)
 
+pca = PCA(train_features.shape[1])
+transformed_features = pca.fit_transform(train_features)
 
+mlp = MLP(hidden_layer_sizes = (2*len(transformed_features)))
+mlp.fit(transformed_features, train_outcome)
+predicted_outcome = mlp.predict(test_features)
+score = accuracy_score(test_outcome, predicted_outcome)
+print(score)
 
