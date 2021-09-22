1f3730cee2e7fa5dda28c6ea43c4f25d16195a54
Kenneth Collins <kenneth.collins@alumni.stonybrook.edu>
2021-09-21 21:16:37 -0400

    running version. Not updated in .py file.

--- Kaggle_HousingPrices.py
+++ Kaggle_HousingPrices.py
@@ -4,46 +4,52 @@ from sklearn.preprocessing import RobustScaler, OrdinalEncoder, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 import pandas as pd
 
-recode:
-LandSlope = {"Gtl": 0, "Mod": 1, "Sev":	2}
-df.loc[:, "LandSlope"] = df.loc[:, "LandSlope"].map(LandSlope)
-df.loc[:, "2ndFlrSF"].fillna(value= 0, inplace=True)
+categorical_features = ["MSZoning",  "Heating", "BldgType", "HouseStyle","Foundation", "GarageType","GarageFinish", "SaleCondition", "SaleType"]
+ordinal_features = ["Street", "Alley", "YearBuilt","YearRemodAdd", "ExterQual", "ExterCond", "CentralAir", "GarageYrBlt", "PavedDrive", "YrSold", "MoSold", "FireplaceQu", "HeatingQC", "KitchenQual", "PoolQC"]
+numerical_features = ["MSSubClass", "LotFrontage", "LotArea", "OverallQual", "OverallCond", "1stFlrSF", "2ndFlrSF", "FullBath", "HalfBath", "TotalBsmtSF"]
 
-
-def recode_likert(df, var_name):
-	condition_map =  {"Ex" = 5, "Gd" = 4, "TA" = 3, "Fa" = 2, "Po" = 1, "NA": 0}
-	return df.loc[:, var_name].map(condition_map)
-
-df.loc[:, "HeatingQC"] = recode_likert(df, "HeatingQC")
-df.loc[:, "FireplaceQu"] = recode_likert(df, "FireplaceQu")
-df.loc[:, "KitchenQual"] = recode_likert(df, "KitchenQual")
-df.loc[:, "PoolQC"] = recode_likert(df, "PoolQC")
-categorical_features = ["MSZoning",  "BldgType", "HouseStyle","Foundation", "GarageType","GarageFinish", "SaleCondition", "SaleType"]
-
-ordinal_features = ["Street", "Alley", "YearBuilt","YearRemodAdd", "ExterQual", "ExterCond", "CentralAir", "HeatingQC", "GarageYrBlt" "PavedDrive", "YrSold", "MoSold", "FireplaceQu", "HeatingQC", "KitchenQual", "PoolQC"]
-
-# recode LandContour: Bnk = 1, HLS = 1, Lvl = 0, Low = -1
-
-numerical_features = ["MSSubClass", "LotFrontage", "LotArea", "OverallQual", "OverallCond", "1stFlrSF", "2ndFlrSF", "Bedroom", "FullBath", "HalfBath"]
-
-df_test = pd.read_csv("")
-df_train = pd.read_csv("")
-
+df_train = pd.read_csv("../input/house-prices-advanced-regression-techniques/train.csv")
+df_test = pd.read_csv("../input/house-prices-advanced-regression-techniques/test.csv")
 train_x = df_train[numerical_features + ordinal_features + categorical_features]
 train_y = df_train["SalePrice"]
 test_x = df_test[numerical_features + ordinal_features + categorical_features]
-test_y = df_test["SalePrice"]
+# test_y = df_test["SalePrice"] # not available
+
+def recode_likert(df, var_name):
+    condition_map =  {"Ex" : 5, "Gd" : 4, "TA" : 3, "Fa" : 2, "Po" : 1, "NA": 0}
+    return df.loc[:, var_name].map(condition_map)
+
+df_test.loc[:, "HeatingQC"] = recode_likert(df_test, "HeatingQC")
+df_test.loc[:, "FireplaceQu"] = recode_likert(df_test, "FireplaceQu")
+df_test.loc[:, "KitchenQual"] = recode_likert(df_test, "KitchenQual")
+df_test.loc[:, "PoolQC"] = recode_likert(df_test, "PoolQC")
+df_test.loc[:, "BsmtCond"] = recode_likert(df_test, "BsmtCond")
+
+df_train.loc[:, "HeatingQC"] = recode_likert(df_train, "HeatingQC")
+df_train.loc[:, "FireplaceQu"] = recode_likert(df_train, "FireplaceQu")
+df_train.loc[:, "KitchenQual"] = recode_likert(df_train, "KitchenQual")
+df_train.loc[:, "PoolQC"] = recode_likert(df_train, "PoolQC")
+df_train.loc[:, "BsmtCond"] = recode_likert(df_test, "BsmtCond")
+
+LandSlope = {"Gtl": 0, "Mod": 1, "Sev":	2}
+
+df_test.loc[:, "LandSlope"] = df_test.loc[:, "LandSlope"].map(LandSlope)
+df_test.loc[:, "2ndFlrSF"].fillna(value= 0, inplace=True)
+
+df_train.loc[:, "LandSlope"] = df_train.loc[:, "LandSlope"].map(LandSlope)
+df_train.loc[:, "2ndFlrSF"].fillna(value= 0, inplace=True)
+
 
 
 numerical_pipeline = Pipeline([
 	("imp", SimpleImputer(strategy= "median")),
-	("enc", RobustScaler)
+	("enc", RobustScaler())
 ])
 
 
 ordinal_pipeline = Pipeline([ 
 	("imp", SimpleImputer(strategy = "constant", fill_value=0)),
-	("enc" OrdinalEncoder()))
+	("enc", OrdinalEncoder(handle_unknown = ‘ignore’))
 ])
 
 categorical_pipeline = Pipeline([
@@ -58,16 +64,16 @@ pre_pipe = ColumnTransformer([
 	("num_pre", numerical_pipeline, numerical_features)
 ])
 
-model = sklearn
+model = sklearn.
 
 full_pipe = Pipeline([
 	("pre", pre_pipe),
 	("model", model)
 ])
-full.fit(train_x, train_y)
-score = full.score(test_x, test_y)
+full_pipe.fit(train_x, train_y)
+score = full_pipe.score(test_x, test_y)
 
-y_hat = full.fit(test_x)
+y_hat = full_pipe.fit(test_x)
 
 submission = pd.DateFrame()
 submission.loc[:, "Id"] = df_test.loc[:, "Id"]
