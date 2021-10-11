c7d78df00c910f31c330c895aeab658f0f9fe41e
Kenneth Collins <kenneth.collins@alumni.stonybrook.edu>
2021-10-11 11:31:20 -0400

    added another solution

--- HackerRankNestedList.py
+++ HackerRankNestedList.py
@@ -23,3 +23,27 @@ if __name__ == '__main__':
     names2.sort()
     for n in names2:
         print(n)
+
+
+# alternative implementation
+import sys
+if __name__ == '__main__':
+    lst = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        lst.append([name, score])
+    lst = sorted(lst, key=lambda x: x[0])
+    lst = sorted(lst, key=lambda x: x[1]) 
+    # sorts are stable, so name order is preserved when sorting by score
+    low = lst[0][1] # get lowest score
+    minsize = -1*sys.maxsize
+    low2 = minsize
+    for i in lst:
+       if low2 > minsize and low2 < i[1]:
+            break
+       elif low2 > minsize and low2 == i[1]:
+            print(i[0])
+       elif low < i[1]: # higher than lowest score
+            low2 = i[1]
+            print(i[0])
